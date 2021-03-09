18fe26bcbdeb32e45be5e7e10c86390c7c635e12
Alejandro <alejandroaguileraalcalde@gmail.com>
2021-03-09 18:42:12 +0100

    transporte con calendario

---
+++ frontend/src/services/Calendar_test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "react-datetime/css/react-datetime.css";
+import Datetime from "react-datetime";
+
+export default class Calendar_test extends React.Component {
+
+    /* Aqui meteremos los componentes que renderizan la página main */
+
+
+    render() {
+
+        return (
+            <div>
+                <Datetime/>
+            </div>
+        );
+    }
+}
