d6287d1a313efd9af1d0dae323dc3c7ab0d6fe5e
Alejandro <alejandroaguileraalcalde@gmail.com>
2021-03-14 13:43:25 +0100

    Transportes actualización

--- frontend/src/services/Transport/Transports.js
+++ frontend/src/services/Transport/Transports.js
@@ -9,30 +9,31 @@ export default class Transports extends React.Component {
 
         let transports = this.props.transports;
         const listItems = transports.map((transport, i) =>
-            <div className="o">
+            <div className="restaurants">
                 <li className={"res_li"}>
 
                     <Link className={"res_link"} to={"/services/transport/show_transport"}
                           onClick={() => this.props.onChangeTransport(i)}>{transport.name}</Link>
                 </li>
             </div>);
+
+
+
         return (
-            <div className="main_Trans">
+            <div className="main_Trans1">
 
+                    <div className="sub_Rest">
 
-
-
-
-                    <div className="sub">
-
-                <h1 className="header_t" >Available Transports:</h1>
+                <h1 className="header_r" >Available Transports:</h1>
                 <ul>{listItems}</ul>
-
             </div>
-
-
-
             </div>
         );
+
+
+
+
+
+
     }
 }
